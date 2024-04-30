46a27961574f733bf5f1d00b4bd83f75117ca084
inmonim <cat1083014@gmail.com>
2024-04-30 12:42:58 +0900

    feat : BE/ CORS 설정

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from controller import quote
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['GET', 'POST', 'PUT'],
+    allow_headers=['*']
+    )
+
 
 app.include_router(quote.router, prefix='/api/v1/quote', tags=['quote'])
 
