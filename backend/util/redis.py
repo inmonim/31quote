a61e2776a8f5c2960e47cd2fcc8097a0f2ea738c
inmonim <cat1083014@gmail.com>
2024-11-10 16:19:49 +0900

    feat : redis 연결실패 대체 로직 임시 생성

--- backend/util/redis.py
+++ backend/util/redis.py
@@ -5,16 +5,17 @@ import redis.asyncio as aioredis
 from DTO import RequestTokenDTO, ResponseQuoteDTO
 from config import REDIS_DB, REDIS_HOST, REDIS_PORT, REFRESH_TOKEN_EXPIRE_MINUTES, ACCESS_TOKEN_EXPIRE_MINUTES
 
-def convert_to_int(value: str | None):
-    if value is None:
-        raise ValueError("None 값은 숫자로 변환할 수 없습니다.")
-    return int(value)
-
 class _R:
     
     def __init__(self):
-        self.__r = aioredis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
-        print("Redis 연결 성공")
+        try:
+            self.__r = aioredis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, socket_timeout=1)
+            self.connect = True
+            print("Redis 연결 성공")
+        except:
+            self.__r = None
+            self.connect = False
+            print("Redis 연결 실패!!!")
 
     async def add_blacklist(self, tokens : RequestTokenDTO) -> bool:
         
